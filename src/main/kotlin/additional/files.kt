46e8d8b47f0cc5de5ac7d4ca29175d86e6246c53
Юрий <93224591+DrraGor@users.noreply.github.com>
2023-05-10 22:09:37 +0300

    TB-2-1 Stage 2 task 1. Создан data класс Word (его бы наверное стоит отдельным файлом сделать). Читаем файл создаём и набиваем объектами Word словарь. Распечатываем в консоль.

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -6,7 +6,25 @@ fun main() {
 
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.readLines().forEach { println(it) }
 
+    val dictionary = mutableListOf<Word>()
+
+    val lines: List<String> = wordsFile.readLines()
+
+    for (i in lines) {
+        val line = i.split("|")
+        dictionary.add(Word(line[0].trim(), line[1].trim(), line[2].trim().toIntOrNull() ?: 0))
+    }
+
+    dictionary.forEach { println(it) }
 }
 
+data class Word(
+    val text: String,
+    val translate: String,
+    val correctAnswer: Int,
+) {
+    override fun toString(): String {
+        return "Word(text= '$text', translate= '$translate', correctAnswer= '$correctAnswer')"
+    }
+}
