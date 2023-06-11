4c8c439bb45720d1344badad66df2dc2755a6a8a
Юрий <93224591+DrraGor@users.noreply.github.com>
2023-06-11 12:32:45 +0300

    TB-9-1 Stage 1

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -1,9 +1,11 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     val botToken = args[0]
     val botService = TelegramBotService(botToken)
     var updateId = 0
     var chatId: String
-    var text: String
+    val trainer = LearnWordsTrainer("words.txt", 3, 4)
 
 
     while (true) {
@@ -11,13 +13,46 @@ fun main(args: Array<String>) {
         val updates: String = botService.getUpdates(updateId)
         println(updates)
 
+
         updateId = (parsing("\"update_id\":(\\d+)".toRegex(), updates) ?: continue).toInt() + 1
 
         chatId = parsing("\"chat\":\\{\"id\":(\\d+)".toRegex(), updates) ?: continue
 
-        text = parsing("\"text\":\"(.+)\"".toRegex(), updates) ?: continue
 
-        botService.sendMessage(chatId, text)
+        when {
+            (parsing("\"text\":\"(.+?)\"".toRegex(), updates)
+                ?: continue).lowercase(Locale.getDefault()) == "/menu" -> botService.sendMenu(chatId)
+
+            (parsing("\"text\":\"(.+?)\"".toRegex(), updates)
+                ?: continue).lowercase(Locale.getDefault()) == "/start" -> botService.sendStartButton(chatId)
+
+            (parsing("\"data\":\"(.+)\"".toRegex(), updates)
+                ?: continue).lowercase(Locale.getDefault()) == "statistics_clicked" -> botService.sendMessage(
+                chatId,
+                "Выучено ${trainer.getStatistics().learned} из ${trainer.getStatistics().total} слов | ${trainer.getStatistics().percent}%"
+            )
+
+            (parsing("\"data\":\"(.+)\"".toRegex(), updates)
+                ?: continue).lowercase(Locale.getDefault()) == "learn_words_clicked" -> {
+                botService.checkNextQuestionAndSend(trainer, botToken, chatId)
+            }
+
+            trainer.checkAnswer(
+                parsing("\"data\":\"answer_(.)\"".toRegex(), updates)?.toInt()
+                    ?: continue
+            ) -> {
+                botService.sendMessage(chatId, "Правильно")
+                botService.checkNextQuestionAndSend(trainer, botToken, chatId)
+            }
+
+            else -> {
+                botService.sendMessage(
+                    chatId,
+                    "Не правильно: ${trainer.question?.correctAnswer?.questionWord} - ${trainer.question?.correctAnswer?.translate}"
+                )
+                botService.checkNextQuestionAndSend(trainer, botToken, chatId)
+            }
+        }
     }
 }
 
@@ -26,3 +61,5 @@ fun parsing(regex: Regex, updates: String): String? {
     val groups = matchResult?.groups
     return groups?.get(1)?.value
 }
+
+
